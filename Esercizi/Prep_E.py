f0b6426b7912c0bd13c16a9e838e6f3c7a1e4a2c
Premarini-Pietro <premarini.pietro@iisgalvanimi.edu.it>
2023-03-08 11:56:10 +0000

    Vers non lo so

---
+++ Esercizi/Prep_E.py
@@ -0,0 +1,43 @@
+import mysql.connector
+ 
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="Animali"
+)
+mycursor = mydb.cursor()
+
+sql = "INSERT INTO Mammiferi ( PersonID, Nome_proprio , Razza , Peso , Eta ) VALUES (%s, %s, %s, %s, %s)"
+
+x = 1
+
+for x in range(5):
+
+
+    PersonID = input("PersonID ") 
+    Nome_proprio = input("Nome_proprio ") 
+    Razza = input("Razza ") 
+    Peso = input("Peso ") 
+    Eta = input("Eta ")
+    val = (PersonID, Nome_proprio, Razza, Peso, Eta)
+
+    print(val)
+    mycursor.execute(sql, val)
+
+
+mydb.commit()
+
+
+print(mycursor.rowcount, "was inserted.")
+
+####################################################################################
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT * FROM Mammiferi where Peso > 2")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+    print(x)
